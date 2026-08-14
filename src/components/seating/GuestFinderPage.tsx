7d8748ba9ae9758e1f524a442a8f7e84c48b433a
HomieEddy <mceddy95@hotmail.fr>
2026-08-14 16:45:02 -0400

    chore: remove unused useEffect import in GuestFinderPage

--- src/components/seating/GuestFinderPage.tsx
+++ src/components/seating/GuestFinderPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from 'react';
+import React, { useMemo, useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import { useQuery, useQueryClient } from '@tanstack/react-query';
 import { motion, AnimatePresence } from 'motion/react';
